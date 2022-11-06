d2f1c11a6515f26de59683b93abf77de8e48158b
oshox <johnnytwobyfour@gmail.com>
2022-11-06 16:38:25 -0700

    Initial Commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 import { Hono } from 'hono'
-import template from "./pages/index.marko"
+import { default as index } from "./pages/index.marko"
 
 const app = new Hono();
 
 app.get('/', (c) => {
   return new Response(
-    template.stream(c), {
+    index.stream(c.req), {
       status: 200,
       headers: { "content-type": "text/html;charset=UTF-8" },
     }
